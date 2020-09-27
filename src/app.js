b3c3798072e2670fa7334c6000bdfd0afe236ed5
Gianni Narracci <gnarracci@gmail.com>
2020-09-27 16:19:23 -0400

    Routes Config

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
+import pkg from '../package.json';
 
 const app = express();
 
+app.set('pkg', pkg);
+
 app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.json({
+        name: app.get('pkg').name,
+        author: app.get('pkg').author,
+        version: app.get('pkg').version,
+        description: app.get('pkg').description
+    });
+})
 
 export default app;
