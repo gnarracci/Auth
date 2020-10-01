696d1004cf9059fbcee46c4d35b69aab18ed3a5b
Gianni Narracci <gnarracci@gmail.com>
2020-09-30 21:07:18 -0400

    Auth Config

--- src/database.js
+++ src/database.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 mongoose.connect("mongodb://localhost/company_db", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: true
 })
     .then(db => console.log('DB is Connected!'))
     .catch(error => console.log(error))
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 import {Router} from 'express';
 const router = Router();
 
-router.post('/signup');
-router.post('/signin');
+import * as authCtrl from '../controllers/auth.controllers';
+
+router.post('/signup', authCtrl.signup);
+router.post('/signin', authCtrl.signin);
 
 export default router;
