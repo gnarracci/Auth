934cf428dbafa40792681b03010ae58ee39486f3
Gianni Narracci <gnarracci@gmail.com>
2020-10-02 00:56:37 -0400

    Signup Setup & Roles Config

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,6 @@
 import User from '../models/User';
+import jwt from 'jsonwebtoken';
+import config from '../config';
 
 export const signup = async (req, res) => {
     const {username, email, password, role} = req.body;
@@ -8,11 +10,17 @@ export const signup = async (req, res) => {
         email,
         password: await User.encryptPassword(password)
     })
-    
-    await newUser.save();
+
+    // Save User
+    const saveUser = await newUser.save();
+
+    // JWT Token
+    const token = jwt.sign({id: saveUser._id}, config.SECRET, {
+        expiresIn: 43200 // Token expire in 12 hours
+    })
 
 
-    res.json('signup');
+    res.json({token});
 }
 
 export const signin = async (req, res) => {
