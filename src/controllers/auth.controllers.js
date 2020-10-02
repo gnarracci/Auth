657cd72eb675e6bd290446010d7ab237c47c5eec
Gianni Narracci <gnarracci@gmail.com>
2020-10-02 11:58:28 -0400

    Auth.controller Main Mods

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,9 +1,10 @@
 import User from '../models/User';
 import jwt from 'jsonwebtoken';
 import config from '../config';
+import Role from '../models/Role';
 
 export const signup = async (req, res) => {
-    const {username, email, password, role} = req.body;
+    const {username, email, password, roles} = req.body;
 
     const newUser = new User({
         username,
@@ -11,8 +12,18 @@ export const signup = async (req, res) => {
         password: await User.encryptPassword(password)
     })
 
+    // Find Roles
+    if (req.body.roles) {
+        const foundRoles = await Role.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)
+    } else {
+        const role = await Role.findOne({name: "user"})
+        newUser.roles = [role._id];
+    }
+
     // Save User
     const saveUser = await newUser.save();
+    console.log(saveUser);
 
     // JWT Token
     const token = jwt.sign({id: saveUser._id}, config.SECRET, {
