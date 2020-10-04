0ea60e1f37d0c52a368763cb4bbf75d628505676
Gianni Narracci <gnarracci@gmail.com>
2020-10-04 01:16:52 -0400

    isBoth check between roles congig

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,9 +4,9 @@ const router = Router();
 import * as productsCtrl from '../controllers/products.controller';
 import {authJwt} from '../middlewares';
 
-router.get('/', [authJwt.verifyToken, authJwt.isUser, authJwt.isAdmin], productsCtrl.getProducts);
+router.get('/', [authJwt.verifyToken, authJwt.isBoth], productsCtrl.getProducts);
 router.post('/', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.createProduct);
-router.get('/:productId', [authJwt.verifyToken, authJwt.isUser, authJwt.isAdmin], productsCtrl.getProductById);
+router.get('/:productId', [authJwt.verifyToken, authJwt.isBoth], productsCtrl.getProductById);
 router.put('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.updateProductById);
 router.delete('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.deleteProductById);
 
