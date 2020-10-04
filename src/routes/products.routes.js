0ecb1c818a3d636af76ec335021ed7b74a1f4f41
Gianni Narracci <gnarracci@gmail.com>
2020-10-04 00:23:06 -0400

    Roles Users Config on Products Routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,9 +4,9 @@ const router = Router();
 import * as productsCtrl from '../controllers/products.controller';
 import {authJwt} from '../middlewares';
 
-router.get('/', [authJwt.verifyToken, authJwt.isUser], productsCtrl.getProducts);
-router.post('/', [authJwt.verifyToken, authJwt.isUser], productsCtrl.createProduct);
-router.get('/:productId', [authJwt.verifyToken, authJwt.isUser], productsCtrl.getProductById);
+router.get('/', [authJwt.verifyToken, authJwt.isUser, authJwt.isAdmin], productsCtrl.getProducts);
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.createProduct);
+router.get('/:productId', [authJwt.verifyToken, authJwt.isUser, authJwt.isAdmin], productsCtrl.getProductById);
 router.put('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.updateProductById);
 router.delete('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.deleteProductById);
 
