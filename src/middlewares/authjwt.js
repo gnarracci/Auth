cf08bf42115c143a1a3494ceab01d29646ee84b4
Gianni Narracci <gnarracci@gmail.com>
2020-10-04 00:15:36 -0400

    Roles Setup Admin

--- src/middlewares/authjwt.js
+++ src/middlewares/authjwt.js
@@ -38,11 +38,27 @@ export const isUser = async (req, res, next) => {
     const user = await User.findById(req.userId);
     const roles = await Role.find({_id: {$in: user.roles}});
 
-    console.log(roles);
-    next();
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "user") {
+            next();
+            return;
+        }
+    }
+
+    return res.status(403).json({message: "Require User Role!"});
 }
 
 
 export const isAdmin = async (req, res, next) => {
+    const user = await User.findById(req.userId);
+    const roles = await Role.find({_id: {$in: user.roles}});
 
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === "admin") {
+            next();
+            return;
+        }
+    }
+
+    return res.status(403).json({message: "Require Admin Role!"});
 }
